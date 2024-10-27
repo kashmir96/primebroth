7fba49e6b9bab74d4ce3bcf5ab9765102c8dedc1
kashmir96 <119819408+kashmir96@users.noreply.github.com>
2024-10-28 09:40:30 +1300

    fixing functions

--- functions/track-event.js
+++ functions/track-event.js
@@ -8,6 +8,9 @@ exports.handler = async (event) => {
     const accessToken = 'EAALoG9CF1ZCYBO3Xx2ZAVAK6Cs2h4XgY55ZA17KQZCGPIXaYlG5NZCP8cXzXZBocH95qb0IGiI22wwZBFRu77fgyDXDHIHi7OhcNjDtXfBnyGNU93mTJDbWD8YMiSZBEk4xgw851GBI1mEMvvMbE7zBHhxOk43akaPmsryIae3XMqQXa44OmPi5gLStAqgTfuKYYvQZDZD'; // Replace with your Facebook Conversions API Access Token
     const pixelId = '809100344281173'; // Replace with your Facebook Pixel ID
 
+    // Optional: Unique event ID for deduplication
+    const eventId = Date.now() + "_" + Math.random();
+
     // Send data to Facebook Conversions API
     const response = await fetch(`https://graph.facebook.com/v13.0/${pixelId}/events?access_token=${accessToken}`, {
       method: 'POST',
@@ -23,6 +26,7 @@ exports.handler = async (event) => {
               value: parseFloat(cartTotal),
             },
             action_source: 'website',
+            event_id: eventId, // Deduplication ID
           },
         ],
       }),
@@ -44,8 +48,3 @@ exports.handler = async (event) => {
     };
   }
 };
-
-
-
-
-
