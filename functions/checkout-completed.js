f194981002d5aefcf5664f17a1a24f31d6e4295e
kashmir96 <119819408+kashmir96@users.noreply.github.com>
2024-11-10 09:57:28 +1300

    updating access token

--- functions/checkout-completed.js
+++ functions/checkout-completed.js
@@ -2,7 +2,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const crypto = require('crypto');  // Import crypto for hashing email
 
 const FACEBOOK_PIXEL_ID = '809100344281173';  // Replace with your Pixel ID
-const ACCESS_TOKEN = 'EAALoG9CF1ZCYBO6KZCXIFjXtCFTInHi3ugWECh2HNKooaRlUoUZBqSHBiN3uoREhZC3FHASfBMWX7A31c5Nv2LKZBKBH2UnCA2OMJZBH9cpEY7O6BOtW5QXCiFi1SnBJzSi3cejTWXBoR4Xc0jkmg6GkrG12VmbWm7jC3MvKQAyplivKBQckzzqYeXaq0yNmi19AZDZD';  // Replace with your Access Token
+const ACCESS_TOKEN = 'EAAU9shr9R4gBO0liRd17Q4qSuPWsaq4O2AT8nbc4SLOwILbfdeGlIIDZArDIfmyCER94PiVToweFgqLca9AYzuVOl4IUfSWZBxcZAcuWnPxzyvnaszTXBvfkGxQBy1N1kQ4Ymb96L7SZCvfJCnRoZAuFIok52rur0B3HOJegQZAMz7ZCiRUyxEQ7ZBxmPS4911Y1cni8PzcZC78ZCkjNFZCOQZDZD';  // Replace with your Access Token
 const webhookSecret = process.env.CHECKOUT_COMPLETED_SECRET;
 
 exports.handler = async (event, context) => {
