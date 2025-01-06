08bc1d8340b97981264fd3955e290d9dc35c1d51
kashmir96 <119819408+kashmir96@users.noreply.github.com>
2025-01-06 13:22:09 +1300

    trying coupon instead

--- functions/create-checkout-session.js
+++ functions/create-checkout-session.js
@@ -107,6 +107,11 @@ exports.handler = async (event, context) => {
       shipping_options: shippingOptions,
       line_items: lineItems,
       mode: 'payment',
+      discounts: [
+        {
+          coupon: '23hSrxmw', // Replace this with your coupon ID
+        },
+      ],
       allow_promotion_codes: true, // Enable promotion codes at checkout
       success_url: successUrl, // Updated success URL
       cancel_url: 'https://www.primalpantry.co.nz/cart/',
