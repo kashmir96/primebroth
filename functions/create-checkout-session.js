0b434422ed261cf12b56227d5260980dc06dff7b
kashmir96 <119819408+kashmir96@users.noreply.github.com>
2025-01-06 13:35:21 +1300

    reverty

--- functions/create-checkout-session.js
+++ functions/create-checkout-session.js
@@ -108,11 +108,6 @@ exports.handler = async (event, context) => {
       line_items: lineItems,
       mode: 'payment',
       allow_promotion_codes: true, // Enable promotion codes at checkout
-      discounts: [
-        {
-          promotion_code: 'promo_1QbZRXFZRwx5tlYmV6Zc83ot', // Replace this with your Stripe Promotion Code ID
-        },
-      ],
       success_url: successUrl, // Updated success URL
       cancel_url: 'https://www.primalpantry.co.nz/cart/',
     });
