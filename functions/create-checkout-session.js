213a4bd9e2ed617fa75c21bcf3e6ddf5b7dbf811
kashmir96 <119819408+kashmir96@users.noreply.github.com>
2025-01-06 13:46:23 +1300

    second fix

--- functions/create-checkout-session.js
+++ functions/create-checkout-session.js
@@ -107,16 +107,11 @@ exports.handler = async (event, context) => {
       shipping_options: shippingOptions,
       line_items: lineItems,
       mode: 'payment',
-      
 
       // Dynamically apply the discount if any item is eligible
   discounts: cart.some(item => item.eligibleForDiscount) // Check eligibility flag
   ? [{ promotion_code: 'promo_1QbZRXFZRwx5tlYmV6Zc83ot' }] // Replace with your Promotion Code ID
   : [], // No discount for ineligible products
-
-
-      
-
 
       success_url: successUrl, // Updated success URL
       cancel_url: 'https://www.primalpantry.co.nz/cart/',
